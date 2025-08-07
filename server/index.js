ed092029c03fe3e6b50ce991fc354b4b24ae2639
Laiba Haque <laibahaque14@gmail.com>
2025-08-07 20:40:46 +0500

    check

--- server/index.js
+++ server/index.js
@@ -56,6 +56,7 @@ app.post("/login", (req, res) => {
 
 // ✅ Final: PORT must be from Railway OR fallback to 5000
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
