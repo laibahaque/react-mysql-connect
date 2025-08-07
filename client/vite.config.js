0f57925c6405556ad6e9bee8df951312cdffc4d8
Laiba Haque <laibahaque14@gmail.com>
2025-08-07 23:21:12 +0500

    vite.config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./", // ✅ Add this line if missing
-  plugins: [react(),tailwindcss(),],
+  base: "./", // ✅ This ensures relative paths work
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // ➕ Makes React Router work on refresh
+          dest: '.' // copied to dist/
+        }
+      ]
+    })
+  ]
 })
