61612dfcd23bf054c5a59a375f3c2d4e2067019e
Laiba Haque <laibahaque14@gmail.com>
2025-08-07 20:26:02 +0500

    fix port

--- server/index.js
+++ server/index.js
@@ -55,6 +55,8 @@ app.post("/login", (req, res) => {
 });
 
 // ✅ Start server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
